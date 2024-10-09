2762a7cd9a2dd8f376a007f832db998719c16ac0
harunisiaho <harunisiaho@gmail.com>
2024-10-09 08:05:12 +0300

    Execute shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -5,7 +5,7 @@ echo "Installing Cowsay
 sudo apt-get install cowsay -y
 
 echo "Execute cowsay CMD"
-cowsay -f dragon "Run for cover, I'm a DRAGON....RAWE" >> gragon.txt
+cowsay -f dragon "Run for cover, I\'m a DRAGON....RAWE" >> gragon.txt
 
 echo "Test file exists"
 grep -i "dragon" gragon.txt
